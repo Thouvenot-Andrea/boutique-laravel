209749d5b0bd389cc9b6812ee072a62d6e1b02e9
thouvenot <thouvenot@thouvenot-desktop>
2024-03-04 15:00:28 +0100

    fix Role : adding  HasUuids next to HasFactory

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,11 +10,15 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     public function users() : \Illuminate\Database\Eloquent\Relations\BelongsToMany
     {
         return $this->belongsToMany(User::class);
+    }
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
     }
 }
 
