1933ea7273abb51bd1cc9b0783c610e7b9073f55
thouvenot <thouvenot@thouvenot-desktop>
2024-03-04 14:58:43 +0100

    fix permission : create

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Permission extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
-
+    public function roles() : \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
