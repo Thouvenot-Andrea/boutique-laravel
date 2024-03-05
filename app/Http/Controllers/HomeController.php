484040bbd1cd13ecbc962e1857510b3acb4cff86
thouvenot <thouvenot@thouvenot-desktop>
2024-03-05 16:40:07 +0100

    feat: Retrieves product and comment information

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,8 +1,26 @@
 <?php
+
 namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Product;
+use Illuminate\View\View;
+use Nette\Utils\Paginator;
+
 class HomeController extends Controller
 {
-    public function index(){
-        return view('homepage');
+    public function index()
+    {
+
+        return view('homepage', [
+            'products' => Product::inRandomOrder()->limit(4)->get(),
+            'comments' => Comment::get(['rating']),
+
+
+
+//            'products' => Product::inRandomOrder()->paginate(2),
+            ]);
     }
+
+
 }
