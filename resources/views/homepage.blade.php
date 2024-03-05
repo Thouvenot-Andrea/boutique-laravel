1e0d25199316faad2cae6cc9ca7e7229289532a0
thouvenot <thouvenot@thouvenot-desktop>
2024-03-05 16:37:54 +0100

    feat: Display of 4 products(image,name and price).

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,8 +6,31 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+<h1 class="text-3xl font-bold underline"></h1>
+<main>
+    {{--        <div class="flex flex-col space-y-10 md:space-y-16 ">--}}
+    <div class="flex flex-row space-x-2 justify-center">
+        @foreach($products as $product )
+            <article>
+                <div>
+                    <img class="object-cover h-[300px] min-w-[290px] max-w-full]" src="{{$product->picture}}">
+                </div>
+                <div class="max-w-[390px]">
+                    <h1 class="text-center text-blue-700">Titre: {{$product->name}}</h1>
+                    <h3 class="text-center">  {{$product->TTC_price}} €</h3>
+                    @foreach($comments as $comment)
+                        <h3>{{$comment->rating}}</h3>
+                    @endforeach
+                </div>
+            </article>
+        @endforeach
+        {
+        {{--            {{$products->links()}}--}}
+
+    </div>
+
+</main>
+
+
 </body>
 </html>
