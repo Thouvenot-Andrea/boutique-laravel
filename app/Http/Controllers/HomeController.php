f1aa971db5888f34aa347b9bc0184794343ecfd4
Vauquelin Aurelie <aurelie.vauquelin@le-campus-numerique.fr>
2024-03-05 15:01:37 +0100

    feat : home/create categories list

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,8 +1,13 @@
 <?php
 namespace App\Http\Controllers;
+use App\Models\Category;
+use Nette\Utils\Paginator;
+
 class HomeController extends Controller
 {
     public function index(){
-        return view('homepage');
+        $categories = Category::all();
+        return view('homepage', compact('categories'));
     }
+
 }
