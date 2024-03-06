f7dcca3e79ef3ddf5911fb75433578496dbf52a0
thouvenot <thouvenot@thouvenot-desktop>
2024-03-06 08:34:46 +0100

    fix:Merger of the homepage with the categories and the flagship product.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,8 +6,30 @@
     @vite('resources/css/app.css')
 </head>
 <body>
+
 <h1 class="text-3xl font-bold underline"></h1>
 <main>
+    <h1 class="text-3xl text-center font-bold mb-10">
+        Bienvenue chez Tempo !
+    </h1>
+
+{{--    <div class="categories">--}}
+{{--        <div class="text-3xl text-center mb-10">--}}
+{{--            <h2>Catégories</h2>--}}
+{{--        </div>--}}
+{{--        <div class="flex flex-wrap justify-center">--}}
+{{--            @foreach($categories as $category)--}}
+{{--                <div>--}}
+{{--                    <div class="mx-3">--}}
+{{--                        <img src='{{$category->image}}'>--}}
+{{--                    </div>--}}
+{{--                    <div class="text-center mt-5 mb-5">--}}
+{{--                        <h3>{{ $category->name }}</h3>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
+{{--    </div>--}}
     {{--        <div class="flex flex-col space-y-10 md:space-y-16 ">--}}
     <div class="flex flex-row space-x-2 justify-center">
         @foreach($products as $product )
@@ -25,11 +47,13 @@
             </article>
         @endforeach
         {
-        {{--            {{$products->links()}}--}}
+                    {{$products->links()}}
 
     </div>
 
 </main>
+
+
 
 
 </body>
