62d4bb81c62d512c5b790ae1bde62988daa1b555
thouvenot <thouvenot@thouvenot-desktop>
2024-03-06 08:35:02 +0100

    fix:Merger of the homeController with the categories and the flagship product.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,25 +2,35 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Models\Comment;
 use App\Models\Product;
 use Illuminate\View\View;
+use App\Models\Category;
 use Nette\Utils\Paginator;
 
 class HomeController extends Controller
 {
+
     public function index()
     {
 
         return view('homepage', [
-            'products' => Product::inRandomOrder()->limit(4)->get(),
+//            'products' => Product::inRandomOrder()->limit(4)->get(),
             'comments' => Comment::get(['rating']),
+//            'categories'=> Category::all(),
 
-
-
-//            'products' => Product::inRandomOrder()->paginate(2),
+           'products' => Product::inRandomOrder()->paginate(2),
             ]);
     }
+
+
+
+//    public function index(){
+//        $categories = Category::all();
+//        return view('homepage', compact('categories'),
+//        );
+//    }
 
 
 }
