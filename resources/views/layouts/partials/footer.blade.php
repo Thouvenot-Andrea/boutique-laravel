1356314664da5490095c4f1b7cda140397d9c0a7
PhilémonMaly <philemon.maly@le-campus-nemrique.fr>
2024-03-06 09:54:55 +0100

    feat layout footer

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,34 @@
+@extends('base')
+
+@section('content')
+
+<footer>
+    <div>
+        <div>
+           <h4>Vos avantages</h4>
+            <p>
+                Garantie 30 jours satisfait ou remboursé<br/>
+                Garantie 3 ans<br/>
+                Qualité garantie<br/>
+
+            </p>
+        </div>
+        <div>
+            <h4>Services</h4>
+            <p>
+                Frais de port et délais de livraison<br/>
+                Nous contacter<br/>
+                CGV / Infos légales<br/>
+                Politique de confidentialité<br/>
+            </p>
+        </div>
+        <div>
+            <h4>Suivez-nous</h4>
+        </div>
+        <div>
+            <h4>Stores</h4>
+        </div>
+    </div>
+</footer>
+
+@endsection
