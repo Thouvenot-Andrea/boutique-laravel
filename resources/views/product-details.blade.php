f135dc77e79ae2ae5c545ed9e18a3c4ab7bb4e41
thouvenot <thouvenot@thouvenot-desktop>
2024-03-07 12:03:40 +0100

    feat: Adding text for accessories and creating comments

--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -4,7 +4,8 @@
     @include('header')
     @include('sidebar')
     <div class="flex flex-col space-y-10 md:space-y-16 ">
-        <h2 class="font-bold  text-3xl text-center">Accessoires</h2>
+        <h2 class="font-bold  text-3xl text-center underline decoration-orange-400">Accessoires</h2>
+        <h2 class="text-center uppercase font-bold">Des musiciens ont acheté en même temps ces références, vous en avez sûrement besoin</h2>
         <div class="flex flex-col space-y-10 md:space-y-16 ">
             <div class="flex flex-wrap space-x-2 justify-center">
                 @foreach ($recommendations as $recommendation)
@@ -15,7 +16,7 @@
                         </div>
                         <div class="max-w-[390px]">
                             <h1 class="text-center text-blue-700">
-                                Titre: {{$recommendation->recommendedProduct->name}}</h1>
+                               {{$recommendation->recommendedProduct->name}}</h1>
                             <h2>{{$recommendation->recommendedProduct->description}}</h2>
                             <h3 class="text-center">{{number_format($recommendation->recommendedProduct->TTC_price/100, 2)}}
                                 €</h3>
@@ -24,5 +25,22 @@
                 @endforeach
             </div>
         </div>
+    </div>
+    <div class="flex flex-col space-y-100 md:space-y-10 my-10 ">
+        <h2 class="font-bold  text-3xl text-center underline decoration-orange-400">Commentaires</h2>
+        <div>
+            <h3 class="flex flex-wrap space-x-2  justify-center text-2xl">{{ $averageRating }}/5</h3>
+            <div class="flex flex-wrap space-x-2  justify-center ">
+
+                @foreach ($comments as $comment)
+                    <article>
+                        <div class="max-w-[390px]  bg-orange-400 text-center rounded-lg m-5 ">
+                            <h3>{{$comment->content}}</h3>
+                        </div>
+                    </article>
+                @endforeach
+            </div>
+        </div>
+    </div>
     @include('footer')
 @endsection
